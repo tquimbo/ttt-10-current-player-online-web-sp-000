bf4d1ccab55535addacc4d44727509b5b0d94dcf
Tim Quimbo <tquimbo1@gmail.com>
2021-02-26 20:55:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board)
-turns = 0
-board.each do |token|
-  if token == "X" || token == "O"
-    turns += 1
+turn = 0
+board.each do |player|
+  if player == "X" || player == "O"
+    turn += 1
   end
 end
 end
